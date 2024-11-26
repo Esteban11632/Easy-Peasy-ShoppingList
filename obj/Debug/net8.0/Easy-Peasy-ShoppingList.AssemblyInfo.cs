734be4c67303bb1f7fed2ddaf7a26273b9c9e925
AdrianQS2004 <adrian.quiros76@gmail.com>
2024-11-25 19:45:37 -0600

    Makes it so that only Admin Users can edit the Shopping List

--- obj/Debug/net8.0/Easy-Peasy-ShoppingList.AssemblyInfo.cs
+++ obj/Debug/net8.0/Easy-Peasy-ShoppingList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Easy-Peasy-ShoppingList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee0683def552a728491a393f3c2b47933e7a934")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54aa1393826f3e10e97dc379faccf8d99ccbee52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Easy-Peasy-ShoppingList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Easy-Peasy-ShoppingList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
